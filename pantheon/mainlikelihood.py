efb187f07b5f6933c136c4e2687a697eb74db991
Adam Stewart <adam@akurate.com.au>
2018-09-24 21:51:28 +1000

    update

--- pantheon/mainlikelihood.py
+++ pantheon/mainlikelihood.py
@@ -20,14 +20,15 @@ print('\n' + 'Importing observational data...')
 
 name = np.loadtxt(lightData,dtype=str,usecols=0)
 zcmb = np.loadtxt(lightData,dtype=float,usecols=1)
-zhel = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=2)
-dz = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=3)
-mb = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=4)
-dmb = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=5)
-x1 = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=6)
-dx1 = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=7)
-color = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=8)
-dcolor = np.loadtxt('lcparam_full_long.txt', dtype=float, usecols=9)
+zcmbArray = np.asarray(zcmb)
+zhel = np.loadtxt(lightData, dtype=float, usecols=2)
+dz = np.loadtxt(lightData, dtype=float, usecols=3)
+mb = np.loadtxt(lightData, dtype=float, usecols=4)
+dmb = np.loadtxt(lightData, dtype=float, usecols=5)
+x1 = np.loadtxt(lightData, dtype=float, usecols=6)
+dx1 = np.loadtxt(lightData, dtype=float, usecols=7)
+color = np.loadtxt(lightData, dtype=float, usecols=8)
+dcolor = np.loadtxt(lightData, dtype=float, usecols=9)
 print('Import successful')
 
 # Build pd data frame
@@ -65,22 +66,42 @@ Model generation
 
 H = 70 #H0
 c = 3e8 #so
-dH = (c / H) * 10 ** -3
+hubDist = (c / H) * 10 ** -3
 
 omegaM = 0.28 #matter density
 omegaL = 0.72 # lambda density
 omegaK = 0 # curvature density
+
+muModelData = []
+
 
 def muModel(z,omegaM,omegaL,omegaK):
     E =  np.sqrt(omegaM * (1 + z) ** (3) + (omegaK * (1 + z) ** 2) + omegaL)
     ## integrates 1/E from 0 to z
     integrand = integrate.quad(lambda z: E ** (-1), 0, z)[0]
     ## luminosity distance = hubble distance * (1 + z) * integrand
-    dL = (dH) * (1 + z) * integrand
-    mu = 5 * np.log10(dL * 10 ** 5)
+    lumDist = hubDist * (1 + z) * integrand
+    mu = 5 * np.log10(lumDist * 10 ** 5) - 19
 
     return mu
 
-muModel = muModel(zcmb,omegaM,omegaL,omegaK)
+for i in range(len(zcmbArray)):
+    mui = muModel(zcmbArray[i],omegaM,omegaL,omegaK)
+    muModelData.append(mui)
 
-print(muModel)
+
+'''
+Parameter space
+'''
+omegaMmin = 0
+omegaMmax = 3
+omegaLmin = 0
+omegaLmax = 3
+
+stepsize = 0.01
+print('\n','Parameters to vary: omegaM, omegaL')
+print('Generating parameter space...')
+print('Step size: ', stepsize)
+
+omegaMpace = np.arange(omegaMmin,omegaMmax,stepsize)
+omegaLspace = np.arange(omegaLmin,omegaLmax,stepsize)

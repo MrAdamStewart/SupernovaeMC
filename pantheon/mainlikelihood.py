4a675a18276e7dc675a75328c5850ad037f576ea
Adam Stewart <adam@akurate.com.au>
2018-10-17 12:13:48 +1100

    working ocdm

--- pantheon/mainlikelihood.py
+++ pantheon/mainlikelihood.py
@@ -5,25 +5,25 @@ import scipy.integrate as integrate
 import random
 import math
 import time
+import os
+from datetime import date
 
+# constants (from Planck 2018)
+h = 0.674
+hubDist = 3000 / h
 
-
+# system info
+todayDate = date.today()
+cwd = os.getcwd()
 # module settings
 np.set_printoptions(threshold=2000000)
 pd.options.display.max_rows = 2000
 
-'''
-Data and covariance matrix importing
-'''
-
-
-# There has to be a simpler way to do this #
 ###DATA###
 lightData="lcparam_full_long.txt"
 print('Data: Pantheon')
 print('Data File: ' + lightData)
 print('\n' + 'Importing observational data...')
-
 name = np.loadtxt(lightData,dtype=str,usecols=0)
 zcmb = np.loadtxt(lightData,dtype=float,usecols=1)
 zcmbArray = np.asarray(zcmb)
@@ -36,56 +36,38 @@ dx1 = np.loadtxt(lightData, dtype=float, usecols=7)
 color = np.loadtxt(lightData, dtype=float, usecols=8)
 dcolor = np.loadtxt(lightData, dtype=float, usecols=9)
 print('Import successful')
-
 # Build pd data frame
 print('\n' + 'Building data frame...')
 snMatrix = np.column_stack((name, zcmb, zhel, dz, mb, dmb, x1, dx1, color, dcolor))
 snDataArray = pd.DataFrame(snMatrix,index=np.arange(1,1049),
     columns=('Name','zCMB','zHel','dz','mb','dmb','x1','dx1','color','dcolor'))
 print('Build successful')
-
 # Display data frame
 print('\n' + 'Total Samples: ' + str(len(snDataArray)))
 print('\n')
 print(snDataArray)
 
 ###COVARIANCE###
-
 # Data file is vector
 print('\n' + 'Importing covariance file...')
 covarianceFile="sys_full_long.txt"
 covarianceData = np.genfromtxt(covarianceFile,unpack=True,skip_header=1)
 print('Import successful')
-
 # Reshape to matrix
 print('Building covariance matrix...')
 # diagonalise magnitude errors
 dStat = np.diag(dmb)
 # C = cSys + dStat (covmat)
 covarianceMatrix = np.reshape(covarianceData, (1048, 1048)) + dStat
-
 print('Build successful')
-
 # Display matrix info
 print('--- Covariance Matrix ---')
 print('Dimensions: ''Columns: ' + str(len(covarianceMatrix)) +'  Rows: ' + str(len(covarianceMatrix[0])))
-
-
-h = 0.7
-
-hubDist = 3000 / h
-
-omegaK = 0 # curvature density
-
-#lumDists = []
 
 def muModel(z,omegaM,omegaL):
     global omegaK
     zz = z
     omegaK = 1 - omegaM - omegaL
-    #E =  np.sqrt(omegaM * (1 + zz) ** 3 + omegaK * (1 + zz) ** 2 + omegaL)
-    ## integrates 1/E from 0 to z
-    #print(1 / E)
     integral = integrate.quad(lambda zz: 1 / (np.sqrt(omegaM * (1 + zz) ** 3 + omegaK * (1 + zz) ** 2 + omegaL)), 0, z)[0]
     comovingDistance = hubDist * integral
     if omegaK == 0:
@@ -95,107 +77,74 @@ def muModel(z,omegaM,omegaL):
     if omegaK < 0:
         omegaKabs = abs(omegaK)
         comovingDistanceTransverse = hubDist * 1 / math.sqrt(omegaKabs) * math.sin(math.sqrt(omegaKabs) * comovingDistance / hubDist)
-
     angularDiameterDistance = comovingDistanceTransverse / (1 + z)
-
     luminosityDistance = (1  + z) ** 2 * angularDiameterDistance
-
     mu = 5 * np.log10(luminosityDistance * 1e5)
 
     return mu
 
-
-#print(muModel(1,0.3,0.7))
-
 def likelihood(zcmbArray,omegaM,omegaL):
-    # build model data
+    #data - modeldata
     muModelData = []
     for i in range(len(zcmbArray)):
         mui = muModel(zcmbArray[i],omegaM,omegaL)
         muModelData.append(mui)
-    # initial residuals for model
     residuals = mb - muModelData
-    # weight of residuals using errors
+    #weight residuals using errors
     weight = []
     for i in range(len(dmb)):
         w = dmb[i] / sum(dmb)
         weight.append(w)
-    # def scriptm
+    #add scriptm to model
     scriptm = sum(residuals*weight)
-    # add scriptm to model
     muModelData = muModelData + scriptm
-    # final residuals with scriptm
     likeResiduals = np.matrix(mb - muModelData)
-    # likelihood value
+    #chi^2
     likeValue = likeResiduals * np.linalg.inv(covarianceMatrix) * likeResiduals.getH()
-    #print('likelihood:', round(float(likeValue),4))
 
     return likeValue
-'''
 
-likerange = [0] * 100
-for i in range(0,100):
-    omM = (0.01)  * i
-    loglik = float(likelihood(zcmbArray,omM,0.7))
-    likerange[i] = loglik
-    print(i)
-
-omegaMrange = np.arange(0,1,0.01)
-
-plt.plot(omegaMrange,likerange,'kx')
-plt.savefig('fig1.png')
-plt.show()
-'''
+#empty arrays to store results
 omegaMx = []
 omegaLy = []
 
-
 def start(omegaMmin,omegaMmax,omegaLmin,omegaLmax,stepsize,iterations):
+    #random start point from given range
     omegaM = random.uniform(omegaMmin,omegaMmax)
     omegaL = random.uniform(omegaLmin,omegaLmax)
-    print('omegam',omegaM)
-    print('omegal',omegaL)
-    l = []
+    print('Starting Values:' + '\n' + 'Omega_M:' + str(round(omegaM,4)) + ' Omega_L: ' + str(round(omegaL,4)))
     for i in range(1,iterations):
         global like
-        global tElapsed
-
         t1 = time.clock()
-
         like1 = likelihood(zcmbArray,omegaM,omegaL)
         loglike1 = np.exp((- 1 / 2) * like1)
-
         jump(omegaM,omegaL,stepsize)
-
         like2 = likelihood(zcmbArray,omegaMnew,omegaLnew)
         loglike2 = np.exp((- 1 / 2) * like2)
-
         likeratio = loglike2 / loglike1
-
         if likeratio > 1:
             like = loglike2
+            chi = like2
             omegaM = omegaMnew
             omegaL = omegaLnew
         else:
-            r = random.uniform(0,1)
+            r = random.random()
             if likeratio < r:
                 like = loglike1
+                chi = like1
                 pass
             else:
                 like = loglike2
+                chi = like2
                 omegaM = omegaMnew
                 omegaL = omegaLnew
-
         if omegaM < 0:
             omegaM = 0.001
-
         omegaMx.append(round(omegaM,7))
         omegaLy.append(round(omegaL,7))
-
         tElapsed = str(round(time.clock() - t1, 2))
-        print(str(i) + ':' + '  M:' + str(round(omegaM,5)) + ' L:' + str(round(omegaL,5)) +
-        ' K:' + str(round(omegaK,4)) + '    likelihood: ' + str(like[0]))
-
+        print(str(i) + ':' + '  M:' + str(round(omegaM,3)) + ' L:' + str(round(omegaL,3)) +
+        ' K:' + str(round(omegaK,3)) + '    chi-squared: ' + str(round(float(chi),3)))
 
 def jump(omegaM,omegaL,stepsize):
     global omegaMnew
@@ -207,23 +156,23 @@ def jump(omegaM,omegaL,stepsize):
 
     return omegaMnew,omegaLnew
 
-iterations = 10000
-stepsize = 0.04
-
 #random start point in between these values
-omegaM_min = 0.2
+omegaM_min = 0.27
 omegaM_max = 0.4
-
 omegaL_min = 0.5
 omegaL_max = 0.8
 
+#start sim
+iterations = 11000
+stepsize = 0.022
 start(omegaM_min,omegaM_max,omegaL_min,omegaL_max,stepsize,iterations)
 
+#save results
 data = np.column_stack((omegaMx,omegaLy))
-np.savetxt('densityparameters.txt', data, header='omega_M , omega_L')
+outputName = cwd + "/runs/" + str(todayDate) + "_" + str(stepsize) + "_" + str(iterations) + ".txt"
+np.savetxt(outputName, data, header='omega_M , omega_L')
 
-###acceptsnce rate ~ 0.3
-
+#summary plot
 plt.plot(omegaMx,omegaLy,'kx')
 plt.xlim((0,1))
 plt.ylim((-1,2))
--- pantheon/plot.py
+++ pantheon/plot.py
@@ -1,10 +1,121 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import math
+import matplotlib.mlab as mlab
+import matplotlib.axes as axes
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
-omegaM = np.loadtxt('omegaM.txt',dtype=float)
-omegaL = np.loadtxt('omegaL.txt',dtype=float)
+print('/n')
 
-fig = plt.figure()
+cwd = os.getcwd()
 
-print(np.mean(omegaL))
-print(np.std(omegaL))
+## ADJUST THIS
+fname = cwd + '/runs/2018-10-16_0.022_11000.txt'
+print('file name: ' + fname)
+oCDM = True # else L-CDM
+burn = 7000
+
+data = np.loadtxt(fname, dtype='float', usecols=(0,1), skiprows=burn)
+
+omegaM = data[:,0]
+omegaL = data[:,1]
+omegaK = 1 - omegaM - omegaL
+
+if oCDM == True:
+    print('Model: o-CDM')
+else:
+    print('Model: L-CDM')
+
+samples = len(data)
+print('Burn = ' + str(burn) + ' Samples = ' + str(samples))
+
+meanM = np.mean(omegaM)
+meanL = np.mean(omegaL)
+meanK = np.mean(omegaK)
+
+stdM = np.std(omegaM)
+stdL = np.std(omegaL)
+stdK = np.std(omegaK)
+
+varM = np.var(omegaM)
+varL = np.var(omegaL)
+varK = np.var(omegaK)
+
+covML = np.cov(omegaM, omegaL)
+
+print('Omega_M: mean = ' + str(round(meanM,5)) + ' std = ' + str(round(stdM,5)) + ' var = ' + str(round(varM,5)))
+print('Omega_L: mean = ' + str(round(meanL,5)) + ' std = ' + str(round(stdL,5)) + ' var = ' + str(round(varL,5)))
+print('Omega_K: mean = ' + str(round(meanK,5)) + ' std = ' + str(round(stdK,5)) + ' var = ' + str(round(varK,5)))
+
+N = 1000
+M = np.linspace(0, 1, N)
+L = np.linspace(-1, 2, N)
+M, L = np.meshgrid(M,L)
+
+mu = np.array([meanM, meanL])
+Sigma = covML
+
+pos = np.empty(M.shape + (2,))
+pos[:, :, 0] = M
+pos[:, :, 1] = L
+
+def multivariate_gaussian(pos, mu, Sigma):
+    """Return the multivariate Gaussian distribution on array pos.
+
+    pos is an array constructed by packing the meshed arrays of variables
+    x_1, x_2, x_3, ..., x_k into its _last_ dimension.
+
+    """
+    n = mu.shape[0]
+    Sigma_det = np.linalg.det(Sigma)
+    Sigma_inv = np.linalg.inv(Sigma)
+    N = np.sqrt((2*np.pi)**n * Sigma_det)
+    # This einsum call calculates (x-mu)T.Sigma-1.(x-mu) in a vectorized
+    # way across all the input variables.
+    fac = np.einsum('...k,kl,...l->...', pos-mu, Sigma_inv, pos-mu)
+
+    return np.exp(-fac / 2) / N
+
+# The distribution on the variables X, Y packed into pos.
+Z = multivariate_gaussian(pos, mu, Sigma)
+normalizedZ = (Z - np.min(Z))/(np.max(Z) - np.min(Z))
+
+plt.figure(1)
+plt.contourf(M, L, normalizedZ, [0.05, 0.45, 1], cmap=cm.winter)
+
+x = np.linspace(0,5,10)
+y1 = 1 - x
+plt.plot(x,y1,'k')
+
+y2 = 0.5 * x
+plt.plot(x,y2,'k--')
+plt.xlim(0,1)
+plt.ylim(0,2)
+plt.xlabel("$\Omega_{\mathrm{M}}$")
+plt.ylabel("$\Omega_{\Lambda}$")
+plt.axes()
+plt.tick_params(direction='in')
+
+step = np.arange(0,len(omegaM))
+
+plt.figure(2)
+plt.plot(step,omegaM)
+plt.title('Random walk')
+plt.ylabel("$\Omega_M$")
+plt.xlabel('Steps (#)')
+
+plt.figure(3)
+plt.plot(step,omegaL)
+plt.title('Random walk')
+plt.ylabel("$\Omega_{\Lambda}$")
+plt.xlabel('Steps (#)')
+
+plt.figure(4)
+plt.plot(step,omegaK)
+plt.title('Random walk')
+plt.ylabel("$\Omega_k$")
+plt.xlabel('Steps (#)')
+
+plt.show()
